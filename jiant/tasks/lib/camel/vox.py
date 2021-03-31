4cf5132af5b7d283cb60bd245e9eb342262fdd1d
Go Inoue <go.inoue@nyu.edu>
2021-03-31 16:29:56 +0400

    Add missing methods

--- jiant/tasks/lib/camel/vox.py
+++ jiant/tasks/lib/camel/vox.py
@@ -9,6 +9,8 @@ from jiant.tasks.core import (
 from jiant.tasks.lib.templates.shared import (
     labels_to_bimap,
 )
+from jiant.utils.python.datastructures import zip_equal
+from jiant.utils.python.io import read_file_lines
 
 from jiant.tasks.lib.udpos import (
     UdposTask,
@@ -64,3 +66,50 @@ class CAMeLvoxTask(UdposTask):
     ]
 
     LABEL_TO_ID, ID_TO_LABEL = labels_to_bimap(LABELS)
+
+
+    @property
+    def num_labels(self):
+        return len(self.LABELS)
+
+    def get_train_examples(self):
+        return self._create_examples(data_path=self.path_dict["train"], set_type="train")
+
+    def get_val_examples(self):
+        return self._create_examples(data_path=self.path_dict["val"], set_type="val")
+
+    def get_test_examples(self):
+        return self._create_examples(data_path=self.path_dict["test"], set_type="test")
+
+    @classmethod
+    def _create_examples(cls, data_path, set_type):
+        curr_token_list, curr_pos_list = [], []
+        data_lines = read_file_lines(data_path, "r", encoding="utf-8")
+        examples = []
+        idx = 0
+        for data_line in data_lines:
+            data_line = data_line.strip()
+            if data_line:
+                if set_type == "test":
+                    line_tokens = data_line.split("\t")
+                    if len(line_tokens) == 2:
+                        token, pos = line_tokens
+                    else:
+                        token, pos = data_line, None
+                else:
+                    token, pos = data_line.split("\t")
+                curr_token_list.append(token)
+                curr_pos_list.append(pos)
+            else:
+                examples.append(
+                    Example(
+                        guid=f"{set_type}-{idx}", tokens=curr_token_list, pos_list=curr_pos_list,
+                    )
+                )
+                idx += 1
+                curr_token_list, curr_pos_list = [], []
+        if curr_token_list:
+            examples.append(
+                Example(guid=f"{set_type}-{idx}", tokens=curr_token_list, pos_list=curr_pos_list)
+            )
+        return examples
